d01d6b8f6a12218388457046b825540cea8b9a07
Phant0m0567 <297956591+Phant0m0567@users.noreply.github.com>
2026-09-20 22:04:10 -0400

    delete theme E

--- windows/src/reducers/wallpaper.js
+++ windows/src/reducers/wallpaper.js
@@ -17,10 +17,6 @@ const walls = [
   "ThemeD/img1.jpg",
   "ThemeD/img2.jpg",
   "ThemeD/img3.jpg",
-  "ThemeE/img0.png",
-  "ThemeE/img1.png",
-  "ThemeE/img2.png",
-  "ThemeE/img3.png",
 ];
 
 const normalizeWallIndex = (value, fallback = 0) => {
@@ -36,7 +32,7 @@ var locked = localStorage.getItem("locked");
 var isLocal = typeof window !== "undefined" && (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1");
 var isLocked = isLocal ? true : locked === null ? true : locked === "true";
 
-const themes = ["default", "dark", "ThemeA", "ThemeB", "ThemeD", "ThemeC", "ThemeE"];
+const themes = ["default", "dark", "ThemeA", "ThemeB", "ThemeD", "ThemeC"];
 
 const defState = {
   themes: themes,
